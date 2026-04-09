113ef6e263f9a3c862a9ba6b6a28e1af53be24a1
Raunak yadav <yadavraunak2004@gmail.com>
2026-04-09 10:50:36 +0530

    fix: rename script entry point to openenv-server, update port to 7860

--- server/app.py
+++ server/app.py
@@ -25,7 +25,7 @@ app = create_app(
 )
 
 def main():
-    uvicorn.run("server.app:app", host="0.0.0.0", port=8000)
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
 
 if __name__ == "__main__":
     main()
